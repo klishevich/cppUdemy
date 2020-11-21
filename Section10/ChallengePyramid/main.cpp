615f1ef93f2a9b1947b1b80d199df9a9f81149d5
Michael Klishevich <busation@gmail.com>
2020-11-21 15:48:15 +0300

    pyramid challenge

---
+++ Section10/ChallengePyramid/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main( )
+{
+    string s0;
+    cout << "Enter a string: ";
+    cin >> s0;
+    cout << "Your string is " + s0 << endl;
+    int pyramid_length = s0.length();
+    cout << "Your string length " << pyramid_length << endl;
+    
+    for (int i {0}; i< pyramid_length; i++)
+    {
+        int padding = pyramid_length - 1 - i;
+        for (int j {0}; j<padding; j++)
+            cout << " ";
+        int symbols = i + 1;
+        for (int j {0}; j<symbols; j++)
+            cout << s0[j];
+        for (int j {symbols-2}; j>=0; j--)
+            cout << s0[j];
+        cout << endl;
+    } 
+    return 0;
+}
